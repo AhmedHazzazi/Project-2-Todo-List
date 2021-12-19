7a7bada30b0ff7361430cf704e9ffdec543e6860
AhmedHazzazi <40734018+AhmedHazzazi@users.noreply.github.com>
2021-12-19 12:18:32 +0300

    finish git hub

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,38 @@
 const express = require("express");
 const app = express();
-const db = require('./db');
-const Todo = require('./todo');
 
-app.get('/', (req, res) => {
-    res.json("GET / is Working");
+const db = require("./db");
+const Todo = require("./todo");
+// console.log(Todo);
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.json("GET / is Working");
 });
 
 app.get("/tasks", (req, res) => {
-    Todo.find({}, (err, data) => {
-        if(err) {
-            console.log("ERROR: ", err);
-        }
-    });
+  Todo.find({}, (err, data) => {
+    if (err) {
+      console.log("ERROR: ", err);
+    } else {
+      res.json(data);
+    }
+  });
 });
 
-app.post()
+app.post("/tasks", (req, res) => {
+  console.log('25:',req.body);
+
+  Todo.create(req.body, (err, newTask) => {
+    if (err) {
+      console.log("ERROR: ", err);
+    } else {
+      res.status(201).json(newTask);
+    }
+  });
+});
 
 app.listen(5000, () => {
-    console.log("SERVER IS WORKING ..");
-})
+  console.log("SERVER IS WORKING ..");
+});
