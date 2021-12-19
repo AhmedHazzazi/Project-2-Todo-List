620bffea0b40f977335751d707375e2684393507
AhmedHazzazi <40734018+AhmedHazzazi@users.noreply.github.com>
2021-12-19 11:09:52 +0300

    udate

--- test.js
+++ test.js
@@ -1,17 +1,1 @@
 // This is For xcvxcvcxvzcxvxc
-// sdsdfhfdsbfbsdbsd
-const ref = useRef(initialValue)
-import React from 'react'
-
-function test() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
-
-export default test
-array.forEach(currentItem => {
-    
-})
