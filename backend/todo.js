47cedc8e15ef147eafd6a8c4c9c0ca3d42c92157
AhmedHazzazi <40734018+AhmedHazzazi@users.noreply.github.com>
2021-12-19 19:23:58 +0300

    finish get and post

--- backend/todo.js
+++ backend/todo.js
@@ -1,11 +1,11 @@
 const {Schema, model} = require("mongoose");
 
-const todoSchema=new Schema({
+const todoSchema = new Schema({
   title: String,
   isCompleted: Boolean
 });
 
 // Model
-const Todo=model('Todo',todoSchema);
+const Todo=model('Todo', todoSchema);
 
 module.exports = Todo;
