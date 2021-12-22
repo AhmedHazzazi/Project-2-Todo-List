115a8b9b17640f75069b8b9eaae2f6a578be17c9
AhmedHazzazi <40734018+AhmedHazzazi@users.noreply.github.com>
2021-12-22 13:07:06 +0300

    finish add and update and delete todo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import axios from "axios";
 import Todo from "./components/Todo";
+import Add from "./components/Add";
 
 export default function App() {
   const [tasks, setTasks] = useState([]);
@@ -25,16 +26,81 @@ export default function App() {
       console.log("ERR: ", err);
     });
   };
-  const mapOverTasks = tasks.map((taskObj, i) => {
-    return <Todo key={i} task={taskObj} />
-  });
+
+  const postNewTodo = (body) => {
+    // console.log("func postNewTodo from APP");
+    // {"title":"task 5","isCompleted": false}
+    axios
+    .post(`http://localhost:5000/tasks`,body)
+    .then((response) => {
+      // console.log('RESPONSE: ', response);
+      console.log("DATA: ", response.data);
+      // setTasks(response.data);
+      getData()
+      // change react hooks state using spread operator
+    })
+    .catch((err) => {
+      console.log("ERR: ", err);
+    });
+  };
+
+  const deleteTodo = (id) => {
+    axios
+    .delete(`http://localhost:5000/tasks/${id}`)
+    //     (`http://localhost:5000/tasks/${id}`)
+    .then((response) => {
+      // console.log('RESPONSE: ', response);
+      console.log("DATA: ", response.data);
+      getData()
+      // change react hooks state using spread operator
+    })
+    .catch((err) => {
+      console.log("ERR: ", err);
+    });
+  };
+
+  const toggleTodo = (id, newStatus) => {
+    axios
+      .put(`http://localhost:5000/tasks/${id}/${newStatus}`)
+      .then((response) => {
+        // console.log('RESPONSE: ', response);
+        console.log("DATA: ", response.data);
+        getData();
+        // change react hooks state using spread operator
+      })
+      .catch((err) => {
+        console.log("ERR: ", err);
+      });
+  };
+  
+  // const mapOverTasks = tasks.map((taskObj, i) => {
+  //   return <Todo key={i} task={taskObj} />
+  // });
+
+  // const mapOverTasks = tasks.map((taskObj, i) => (
+  //   <Todo key={i} task={taskObj} deleteTodo={deleteTodo}/>
+  // ));
+  
+  const mapOverTasks = tasks.map((taskObj, i) => (
+    <Todo
+      key={i}
+      task={taskObj}
+      deleteTodo={deleteTodo}
+      toggleTodo={toggleTodo}
+    />
+  ));
   
   return (
     <div className='app'>
       <p>app</p>
       {/* when click on this button 
       should call function bring Data */}
+      <div>ADD TODO</div>
+      <Add createFunc={postNewTodo} />
+      
       <button onClick={getData}>GET TASKS</button>
+      
+      <div>TODO LIST</div>
       {mapOverTasks}
     </div>
   );
