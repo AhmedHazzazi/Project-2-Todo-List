0470fedb13ac515e12dec756101ad18a3bda85cb
AhmedHazzazi <40734018+AhmedHazzazi@users.noreply.github.com>
2021-12-23 11:56:17 +0300

    Update Add.js

--- frontend/src/components/Add.js
+++ frontend/src/components/Add.js
@@ -13,20 +13,22 @@ export default function Add(props) {
   };
 
   return (
-    <form className="row row-cols-lg-auto g-3 align-items-center">
-      <div className="col-12">
-      <label className="" for="txt_title">Title:</label>
-        <input id="txt_title" type="text" className="form-label" placeholder="Write new title here ..." onChange={(e) => { setNewTitle(e.target.value); }} />
-      </div>
+    <div className="Add my-3">
+        <form className="row row-cols-lg-auto g-3 align-items-center">
+          <div className="col-12">
+          <label className="" for="txt_title">Title:</label>
+            <input id="txt_title" type="text" className="form-label mx-2" placeholder="Write new title here ..." onChange={(e) => { setNewTitle(e.target.value); }} />
+          </div>
 
-      <div className="col-12">
-        <input id="isCompletedChecked" type="checkbox" value="" className="form-check-input" onChange={(e) => { setisCompleted(e.target.checked.value ? false : true); }} />
-        <label class="form-check-label" for="isCompletedChecked">isCompleted</label>
-      </div>
+          {/* <div className="col-12">
+            <input id="isCompletedChecked" type="checkbox" value="" className="form-check-input" onChange={(e) => { setisCompleted(e.target.checked.value ? false : true); }} />
+            <label class="form-check-label" for="isCompletedChecked">isCompleted</label>
+          </div> */}
 
-      <div className="col-12">
-        <button className="btn btn-block btn-success mt-3" onClick={createNewTodo}>Create New Todo</button>
-      </div>
-    </form>
+          <div className="col-12 my-3 mx-3">
+            <button className="btn btn-block btn-success mt-2 mx-3" onClick={createNewTodo}>Create New Todo</button>
+          </div>
+      </form>
+    </div>
   );
 }
